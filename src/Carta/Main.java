b138dea986ba3f3a265834e872af37f15664d310
JosemSie <36673200+JosemSie@users.noreply.github.com>
2018-10-27 01:16:31 +0200

    Añadida la gui para seleccionar cartas

--- src/Carta/Main.java
+++ src/Carta/Main.java
@@ -2,13 +2,17 @@ package Carta;
 
 import java.io.IOException;
 import vista.mainView;
+import vista.vistaRangos;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		
-        if(args[0].equals("v")) new mainView().setVisible(true);
+        if(args[0].equals("v")) { 
+        	new mainView().setVisible(true);
+        	new vistaRangos().setVisible(true);
+        }
 		else
 			try {
 				int apartado = Integer.parseInt(args[0]);
