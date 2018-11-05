78d550b1d6585e698f9fa409867d3f3ec8a21488
Ricardo <ricardo.arranz279@outlook.com>
2018-11-05 20:49:49 +0100

    Añade rangos
    Añado las clases de Rango Tablero y coordenadas y un ¡main!

---
+++ src/TheRango/Main.java
@@ -0,0 +1,21 @@
+package TheRango;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Tablero table = new Tablero(new Rang(new String[]{"JJ+", "ATs-A2s"}));
+		System.out.print(table.toString());
+		table.selectCord(8, 8);
+		table.selectCord(12, 9);
+		System.out.print(table.toString());
+		table.selectCord(11, 12);
+		table.selectCord(10, 12);
+		table.selectCord(9, 12);
+		table.selectCord(8, 12);
+		table.selectCord(0, 11);
+		table.selectCord(0, 12);
+		System.out.print(table.toString());
+		Tablero table2 = new Tablero(new Rang(new String[] {"TT+", "AJs-A2s", "K2o", "ATo+", "A2o"}));
+		System.out.print(table2.toString());		
+	}
+}
