b0faa9181cfb1d6280e4878120810d4a3375ce36
Ricardo <ricardo.arranz279@outlook.com>
2018-11-30 14:47:15 +0100

    Cambia nombres
    me he dado cuenta de que en vez de añadir las posibles mesas a una lista y luego recorrer la lisa podriamos hacer que cada vez que conseguimos una posible mesa la probamos con las manos y apuntamos la puntuacion a quien deba... lo he dejado explicado para discutirlo mas tarde, ademas he comentado la solucion actucal en pseudocodigo

--- src/modelo/Equities.java
+++ src/modelo/Equities.java
@@ -10,8 +10,8 @@ public class Equities {
 	private ArrayList<Carta> cartasMesa;
 	private boolean[][] cartasMarc;//las cartas que esten marcadas estaran a true
 	private Carta[][] cartasJugadores;//las 2 cartas de cada jugador
-	private ArrayList<Carta> cartaInsertb;//grupos de 5 cartas que juntaremos con las distintas manos
-	private Carta[] mesaInsert;
+	private ArrayList<Carta> cartaInsertb;//grupos de 5 cartas que reresentan las posibles manos
+	private Carta[] posibleMesa;//aray de NCARTASMESA que ira iremos insertando en cartaInsertb
 	
 	private int[] probsWinPlayer;//resultado final
 	private int[] puntaje;//la usaremos para contar el numero de manos que gana
@@ -41,31 +41,50 @@ public class Equities {
 						[conver.paloAInt(this.cartasJugadores[i][j].getPalo())] = true;
 			}
 		
+		generaCartaInsert();
+		/*int z = this.cartaInsertb.size()/NCARTASMESA;
+		for (int i=0;i<z;i++) {
+			//generamos lista de mejorcartas
+			for(int j=i*NCARTASMESA;j<NCARTASMESA; j++) {//llenamos la posible mesa con las 5 cartas
+				//mesaAux.add(this.cartaInsertb.get(j));
+				//
+			}
+			for (int j=0;j<6;j++) {
+				jugAux = new ArrayList<Carta>();
+				jugAux.addAll(mesaAux);
+				jugAux.add(this.cartasJugadores[i][0]);
+				jugAux.add(this.cartasJugadores[i][1]);
+				listaMejoresJugadas.add(jugAux);
+			}
+			//el mejor jugador anota punto, o los dos mejores 0,5, o ...
+		}
+		*/
 		//generate
 	}
 	
-	public void generaCartaInsert() {//devolvera una lista de todas permitaciones de 5 cartas permitidas	
+	public void generaCartaInsert() {//genera la lista de todas permutaciones de NCARTASMESA cartas permitidas	
 		int k = this.cartasMesa.size();
 		this.cartaInsertb = new ArrayList<Carta>();
-		this.mesaInsert = new Carta[NCARTASMESA];//el array de cartas que vamos a insertar n veces,
+		this.posibleMesa = new Carta[NCARTASMESA];//el array de cartas que contendra la permutacion 
 		for(int i=0;i<this.cartasMesa.size();i++)
-			this.mesaInsert[i] = this.cartasMesa.get(i);
+			this.posibleMesa[i] = this.cartasMesa.get(i);
 		
 		generaCartaInserRecursiv(k, 0, 0);
 	}
 	
-	//genera la lista 5 en cinco cartas de todas la baraja que aun no ha sido repartida
-	//k = numero de cartas ya en la mesa, p= el palo de la ultima carta insertada, el numero de la ultima carta insertada
-	private void generaCartaInserRecursiv(int k, int p, int v){//solo variaran las que no son de la mesa inicial, por lo que pasando la proxima posicion a cambiar de un array nos ahoramos mucha memoria 
-		if(k==NCARTASMESA) {//cuando hemos generado una mesa la añadimos
-			this.cartaInsertb.addAll(new  ArrayList<>(Arrays.asList(this.mesaInsert)));
+	//genera la lista NCARTASMESA en NCARTASMESA cartas de todas la baraja que aun no ha sido repartida
+	//k = numero de cartas ya en la posibleMesa, p = el palo de la ultima carta insertada, v = el valor de la ultima carta insertada
+	private void generaCartaInserRecursiv(int k, int p, int v){//solo variaran las que NO son de la mesa inicial, por lo que pasando la proxima posicion a cambiar de un array nos ahoramos mucha memoria 
+		if(k==NCARTASMESA) {//cuando hemos generado una posible mesa 
+			this.cartaInsertb.addAll(new  ArrayList<>(Arrays.asList(this.posibleMesa)));//la añadimos a la lista de conjuntos de manos
+			//AQUI PODRIAMOS PROBAR DIRECTAMENTE CON TODAS LAS MANOS QUE HAY JUGANDO Y VER QUIEN SE LLEVA EL PUNTO pero puede que quede un poco lioso
 		}
 		else {//en caso de no haber llenado toda la mesa seguimos llenando con cartas aun no utilizadas
 			//siempre contaremos a partir de la siguiente carta que ha sido insertada
 				for(int i=v+1; i<13;i++) {//23456789tjqka
 					for(int j=p+1; j<4;j++) {//las cartas se intentaran insertar desde 2h, 2d... hasta ...Ac, As
 						if(!this.cartasMarc[i][j]) {
-							this.mesaInsert[k] = new Carta(i+2,conver.intAPalo(j));
+							this.posibleMesa[k] = new Carta(i+2,conver.intAPalo(j));
 							generaCartaInserRecursiv(k+1, j, i);
 						}
 					}
