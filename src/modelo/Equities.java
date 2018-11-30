c3d3c7188dd804257db846b1948ed9e97dab9935
Ricardo <ricardo.arranz279@outlook.com>
2018-11-30 14:20:07 +0100

    Crea la clase Equities de la practica 3
    por ahora marca las cartas de las manos y mesa para no repetirlas y genera una lista con todos los grupos de 5 cartas que puede haber de mesa, faltaria generar por cada grupo de 5cartas un bucle que busque que mano gana

---
+++ src/modelo/Equities.java
@@ -0,0 +1,86 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Equities {
+	
+	private static final int NCARTASMESA = 5;	
+	private Conversor conver;
+	private ArrayList<Carta> cartasMesa;
+	private boolean[][] cartasMarc;//las cartas que esten marcadas estaran a true
+	private Carta[][] cartasJugadores;//las 2 cartas de cada jugador
+	private ArrayList<Carta> cartaInsertb;//grupos de 5 cartas que juntaremos con las distintas manos
+	private Carta[] mesaInsert;
+	
+	private int[] probsWinPlayer;//resultado final
+	private int[] puntaje;//la usaremos para contar el numero de manos que gana
+	
+	
+	public Equities() {
+		this.cartasMesa= new ArrayList<Carta>();
+		this.cartasMarc= new boolean[14][4];
+		this.cartasJugadores = new Carta[6][2];
+		this.probsWinPlayer = new int[6];
+		this.puntaje = new int[6];
+		this.cartaInsertb = new ArrayList<Carta>();
+	}
+	
+	public void start() {
+		this.cartasMarc= new boolean[13][4];//23456789TJQKA
+		this.probsWinPlayer = new int[] {0,0,0,0,0,0};
+		this.puntaje = new int[] {0,0,0,0,0,0};
+		
+		for(Carta c : this.cartasMesa) {//marcamos las mesas de la mesa si las hay
+			this.cartasMarc[c.getValor()][conver.charAValor(c.getPalo())] = true;
+		}
+		
+		for(int i=0;i<6;i++)
+			for(int j=0;j<2;j++) {//recorremos todas las cartas de todos los jugadores y las marcanmos en las cartasMarc
+				this.cartasMarc[this.cartasJugadores[i][j].getValor()-2]
+						[conver.paloAInt(this.cartasJugadores[i][j].getPalo())] = true;
+			}
+		
+		//generate
+	}
+	
+	public void generaCartaInsert() {//devolvera una lista de todas permitaciones de 5 cartas permitidas	
+		int k = this.cartasMesa.size();
+		this.cartaInsertb = new ArrayList<Carta>();
+		this.mesaInsert = new Carta[NCARTASMESA];//el array de cartas que vamos a insertar n veces,
+		for(int i=0;i<this.cartasMesa.size();i++)
+			this.mesaInsert[i] = this.cartasMesa.get(i);
+		
+		generaCartaInserRecursiv(k, 0, 0);
+	}
+	
+	//genera la lista 5 en cinco cartas de todas la baraja que aun no ha sido repartida
+	//k = numero de cartas ya en la mesa, p= el palo de la ultima carta insertada, el numero de la ultima carta insertada
+	private void generaCartaInserRecursiv(int k, int p, int v){//solo variaran las que no son de la mesa inicial, por lo que pasando la proxima posicion a cambiar de un array nos ahoramos mucha memoria 
+		if(k==NCARTASMESA) {//cuando hemos generado una mesa la añadimos
+			this.cartaInsertb.addAll(new  ArrayList<>(Arrays.asList(this.mesaInsert)));
+		}
+		else {//en caso de no haber llenado toda la mesa seguimos llenando con cartas aun no utilizadas
+			//siempre contaremos a partir de la siguiente carta que ha sido insertada
+				for(int i=v+1; i<13;i++) {//23456789tjqka
+					for(int j=p+1; j<4;j++) {//las cartas se intentaran insertar desde 2h, 2d... hasta ...Ac, As
+						if(!this.cartasMarc[i][j]) {
+							this.mesaInsert[k] = new Carta(i+2,conver.intAPalo(j));
+							generaCartaInserRecursiv(k+1, j, i);
+						}
+					}
+				}
+				 
+		}
+	}//coste lineal en funcion de todas las permutaciones posibles
+	
+	
+	public void setPlayerCarts(Carta c, int jugador, int posi) {//setea una carta de un jugador siempre que no este en la mesa o en la mano de otro jugador
+		int v = c.getValor()-2, p = conver.charAValor(c.getPalo());
+		if (!this.cartasMarc[v][p]) {
+			this.cartasJugadores[jugador][posi]=c;
+			this.cartasMarc[v][p]=true;
+		}
+	}
+
+}
